a349936ccf1e628905ed06f852ce1667a23aebd6
techwithanirudh <70373803+techwithanirudh@users.noreply.github.com>
2023-08-10 13:57:55 +0000

    feat: Add image command handler  This commit adds a new command handler for handling image-related commands. It implements the logic to generate an image using the OpenAI API based on a given prompt question and posts the generated image in the chat channel.

--- utils/commands.js
+++ utils/commands.js
@@ -1,7 +1,15 @@
 import { event } from "./logging";
 import { isUserStaff, postMessage } from "./messageHandler";
+import { Configuration, OpenAIApi } from 'openai';
+import { OPENAI_API_KEY, OPENAI_BASE_URL } from '../config';
 
 console.event = event;
+
+const configuration = new Configuration({
+    apiKey: OPENAI_API_KEY,
+    basePath: OPENAI_BASE_URL,
+});
+const openai = new OpenAIApi(configuration);
 
 // Define command map
 const commands = {
@@ -39,6 +47,17 @@ async function handleSuspend(_question, CHANNEL_NAME, CHANNEL_ID) {
     process.exit();
 }
 
+async function handleImage(question, CHANNEL_NAME, CHANNEL_ID) {
+    const response = await openai.createImage({
+        prompt: question,
+        n: 1,
+        size: "1024x1024",
+    });
+    const image_url = response.data.data[0].url;
+
+    const markdown = `![Generated image](${image_url})`;
+    await postMessage(markdown, CHANNEL_NAME, CHANNEL_ID);
+}
 
 // Function to check for commands
 async function checkForCommand(question, CHANNEL_NAME, CHANNEL_ID) {
@@ -48,13 +67,14 @@ async function checkForCommand(question, CHANNEL_NAME, CHANNEL_ID) {
         if (question.text.startsWith(prefix)) {
             const command = question.text.slice(1).split(' ')[0]; // Extract the command name
             const commandInfo = commands[command]; // Get the corresponding command info
+            const parsedQuestion = question.text.slice(1 + command.length + 1); // Extract the question
 
             if (commandInfo) {
                 if (commandInfo.staffOnly && !(await isUserStaff(question.author))) {
                     console.event('PERMISSION_ERR', 'User does not have permission to execute this command.');
                     await postMessage('[ERROR] User does not have permission to execute this command.', CHANNEL_NAME, CHANNEL_ID);
                 } else {
-                    await commandInfo.handler(question.text, CHANNEL_NAME, CHANNEL_ID);
+                    await commandInfo.handler(parsedQuestion, CHANNEL_NAME, CHANNEL_ID);
                     return true;
                 }
             } else {
